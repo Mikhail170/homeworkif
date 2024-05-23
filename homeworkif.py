13cb29e1f2a3193209c00a80b468e42589abcd43
Mikhail170 <ga_pe@mail.ru>
2024-05-23 11:35:21 +0200

    Add files via upload

---
+++ homeworkif.py
@@ -0,0 +1,24 @@
+x = 45
+print('Hello')
+if x < 0:
+    print('smaller than zero')
+print('Goodbye')
+a,b = 10,5
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('success')
+if (a > b) and (a >0 or b >1000):
+    print('success')
+if '34' > '123':
+    print('success')
+if '123' > '12':
+    print('success')
+if [1,2]> [1,1]:
+    print('success')
+#if '6' > 5:
+ #   print('success')
+#if [5,6] > 5:
+#    print('success')
+if '44' != 5:
+    print('success')
